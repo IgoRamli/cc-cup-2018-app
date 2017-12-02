e1e64375d1062a6f326ec73f3cc46bbdb141473a
Inigo Ramli <igoramli.igo@gmail.com>
2017-12-02 22:20:07 +0700

    Mengubah kode jadwal lomba menjadi Fragment

---
+++ app/src/main/java/org/osiskanisius/cccup/cccup2018/jadwal_lomba/JadwalFragment.java
@@ -0,0 +1,53 @@
+package org.osiskanisius.cccup.cccup2018.jadwal_lomba;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.osiskanisius.cccup.cccup2018.R;
+
+public class JadwalFragment extends Fragment {
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+    public JadwalFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment EventFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static JadwalFragment newInstance() {
+        JadwalFragment fragment = new JadwalFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_jadwal, container, false);
+        return view;
+    }
+}
