9bef492dacbf43ca3bcf464e80f6552f60c24ce5
Inigo Ramli <igoramli.igo@gmail.com>
2017-12-12 23:51:23 +0700

    Merapikan susunan fragment di Bottom Navigation Bar

--- app/src/main/java/org/osiskanisius/cccup/cccup2018/MainActivity.java
+++ app/src/main/java/org/osiskanisius/cccup/cccup2018/MainActivity.java
@@ -9,11 +9,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import org.osiskanisius.cccup.cccup2018.home.HomeFragment;
 import org.osiskanisius.cccup.cccup2018.jadwal.JadwalFragment;
 
 public class MainActivity extends AppCompatActivity {
     Fragment mainFragment;
     FragmentManager fragmentManager;
+
+    HomeFragment home;
+    JadwalFragment jadwal;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -22,6 +26,16 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
+                    if(home == null){
+                        home = new HomeFragment();
+                    }
+                    fragmentManager.beginTransaction().replace(R.id.main_fragment, home).commit();
+                    return true;
+                case R.id.navigation_jadwal:
+                    if(jadwal == null){
+                        jadwal = new JadwalFragment();
+                    }
+                    fragmentManager.beginTransaction().replace(R.id.main_fragment, jadwal).commit();
                     return true;
             }
             return false;
@@ -32,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getFragmentManager();
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
