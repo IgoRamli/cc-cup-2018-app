f371f1b767955a84799c4de6bccbe07269859122
Inigo Ramli <igoramli.igo@gmail.com>
2017-12-18 00:30:04 +0700

    Menyambungkan jadwal dengan DetailActivity

--- app/src/main/java/org/osiskanisius/cccup/cccup2018/adapter/JadwalRecyclerViewAdapter.java
+++ app/src/main/java/org/osiskanisius/cccup/cccup2018/adapter/JadwalRecyclerViewAdapter.java
@@ -1,11 +1,14 @@
 package org.osiskanisius.cccup.cccup2018.adapter;
 
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.osiskanisius.cccup.cccup2018.DetailActivity;
 import org.osiskanisius.cccup.cccup2018.R;
 
 /**
@@ -15,12 +18,24 @@ import org.osiskanisius.cccup.cccup2018.R;
 public class JadwalRecyclerViewAdapter
         extends RecyclerView.Adapter<JadwalRecyclerViewAdapter.JadwalViewHolder> {
     String[] jadwalData;
-
+    private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent gotoDetail = new Intent(view.getContext(), DetailActivity.class);
+            TextView tv = (TextView) view.findViewById(R.id.tv_jadwal);
+            String data = tv.getText().toString();
+            gotoDetail.putExtra(Intent.EXTRA_TEXT, data);
+            view.getContext().startActivity(gotoDetail);
+            //Toast.makeText(view.getContext(), "Item selected", Toast.LENGTH_LONG).show();
+        }
+    };
+    
     @Override
     public JadwalViewHolder onCreateViewHolder(ViewGroup viewGroup, int i){
         LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
         View view = inflater.inflate(R.layout.jadwal_list_item, viewGroup, false);
         JadwalViewHolder viewHolder = new JadwalViewHolder(view);
+        view.setOnClickListener(mOnClickListener);
 
         return viewHolder;
     }
