d03f1a264add81109f7aa9524c8d601f2d072433
Inigo Ramli <igoramli.igo@gmail.com>
2017-12-23 15:19:05 +0700

    Membuat Data Class menjadi Parcelable

--- app/src/main/java/org/osiskanisius/cccup/cccup2018/data/Lomba.java
+++ app/src/main/java/org/osiskanisius/cccup/cccup2018/data/Lomba.java
@@ -1,19 +1,25 @@
 package org.osiskanisius.cccup.cccup2018.data;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by inigo on 22/12/17.
  */
 
-public class Lomba {
+public class Lomba implements Parcelable{
     private String namaLomba = null;
     private Date waktuMulai = null;
     private ArrayList<LombaDetails> peserta = new ArrayList<>();
-    private static final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss");
+    private static final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss", Locale.US);
+
+    public Lomba(){}
 
     public Lomba(String namaLomba, String waktuMulai){
         this.namaLomba = namaLomba;
@@ -23,6 +29,28 @@ public class Lomba {
             e.printStackTrace();
         }
     }
+
+    protected Lomba(Parcel in) {
+        try{
+            namaLomba = in.readString();
+            waktuMulai = formatter.parse(in.readString());
+            peserta = in.createTypedArrayList(LombaDetails.CREATOR);
+        }catch (ParseException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static final Creator<Lomba> CREATOR = new Creator<Lomba>() {
+        @Override
+        public Lomba createFromParcel(Parcel in) {
+            return new Lomba(in);
+        }
+
+        @Override
+        public Lomba[] newArray(int size) {
+            return new Lomba[size];
+        }
+    };
 
     public String getNamaLomba(){
         if(namaLomba == null){
@@ -54,5 +82,17 @@ public class Lomba {
 
     public void setWaktuMulai(String waktuMulai) throws ParseException{
         this.waktuMulai = formatter.parse(waktuMulai);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(this.namaLomba);
+        parcel.writeString(this.waktuMulai.toString());
+        parcel.writeList(this.peserta);
     }
 }
--- app/src/main/java/org/osiskanisius/cccup/cccup2018/data/LombaDetails.java
+++ app/src/main/java/org/osiskanisius/cccup/cccup2018/data/LombaDetails.java
@@ -1,19 +1,42 @@
 package org.osiskanisius.cccup.cccup2018.data;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by inigo on 22/12/17.
  */
 
-public class LombaDetails {
+public class LombaDetails implements Parcelable{
     private String namaPeserta = null;
     private String namaSekolah = null;
     private int skorPeserta = -1;
+
+    public LombaDetails(){}
+
+    public LombaDetails(Parcel in){
+        this.namaPeserta = in.readString();
+        this.namaSekolah = in.readString();
+        this.skorPeserta = in.readInt();
+    }
 
     public LombaDetails(String namaPeserta, String namaSekolah, int skorPeserta){
         this.namaPeserta = namaPeserta;
         this.namaSekolah = namaSekolah;
         this.skorPeserta = skorPeserta;
     }
+
+    public static final Creator<LombaDetails> CREATOR = new Creator<LombaDetails>() {
+        @Override
+        public LombaDetails createFromParcel(Parcel in) {
+            return new LombaDetails(in);
+        }
+
+        @Override
+        public LombaDetails[] newArray(int size) {
+            return new LombaDetails[size];
+        }
+    };
 
     public String getNamaPeserta(){
         if(namaPeserta == null){
@@ -51,5 +74,17 @@ public class LombaDetails {
     public LombaDetails setSkorPeserta(int skorPeserta){
         this.skorPeserta = skorPeserta;
         return this;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(this.namaPeserta);
+        parcel.writeString(this.namaSekolah);
+        parcel.writeInt(this.skorPeserta);
     }
 }
