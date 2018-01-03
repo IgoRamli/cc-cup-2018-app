679ea104fcba3d4eff3d1b2bfd25484cd5e62502
Inigo Ramli <igoramli.igo@gmail.com>
2018-01-03 23:17:14 +0700

    Menambahkan fitur peta (Stable?)

--- app/src/main/java/org/osiskanisius/cccup/cccup2018/map/MapFragment.java
+++ app/src/main/java/org/osiskanisius/cccup/cccup2018/map/MapFragment.java
@@ -1,20 +1,49 @@
 package org.osiskanisius.cccup.cccup2018.map;
 
 
+import android.Manifest;
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Point;
+import android.graphics.PointF;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Looper;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.davemorrissey.labs.subscaleview.ImageSource;
 import com.indooratlas.android.sdk.IALocation;
 import com.indooratlas.android.sdk.IALocationListener;
 import com.indooratlas.android.sdk.IALocationManager;
 import com.indooratlas.android.sdk.IALocationRequest;
+import com.indooratlas.android.sdk.IARegion;
+import com.indooratlas.android.sdk.resources.IAFloorPlan;
+import com.indooratlas.android.sdk.resources.IALatLng;
+import com.indooratlas.android.sdk.resources.IAResourceManager;
+import com.indooratlas.android.sdk.resources.IAResult;
+import com.indooratlas.android.sdk.resources.IAResultCallback;
+import com.indooratlas.android.sdk.resources.IATask;
+import com.squareup.picasso.Picasso;
 
 import org.osiskanisius.cccup.cccup2018.R;
+
+import java.io.File;
+import java.util.logging.Logger;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,8 +52,14 @@ import org.osiskanisius.cccup.cccup2018.R;
  */
 public class MapFragment extends Fragment {
     private static final String TAG = "MapFragment";
+    private static final int REQUEST_CODE_WRITE_EXTERNAL_STORAGE = 1;
+
     //IndoorAtlas
+    private static final float dotRadius = 1.0f;
     private IALocationManager mLocationManager;
+    private IAResourceManager mResourceManager;
+    private DownloadManager mDownloadManager;
+    private IAFloorPlan mFloorPlan;
     private IALocationListener mIALocationListener = new IALocationListener() {
         // Called when the location has changed.
         @Override
@@ -33,9 +68,13 @@ public class MapFragment extends Fragment {
             Log.d(TAG, "Longitude: " + location.getLongitude());
             Log.d(TAG, "Floor number: " + location.getFloorLevel());
 
-            mLog.append("Latitude: " + location.getLatitude() + "\n");
-            mLog.append("Longitude: " + location.getLongitude() + "\n");
-            mLog.append("Floor number: " + location.getFloorLevel() + "\n\n");
+            if (mImageView != null && mImageView.isReady()) {
+                IALatLng latLng = new IALatLng(location.getLatitude(), location.getLongitude());
+                PointF point = mFloorPlan.coordinateToPoint(latLng);
+                mImageView.setDotCenter(point);
+                mImageView.postInvalidate();
+            }
+
         }
 
         @Override
@@ -43,8 +82,23 @@ public class MapFragment extends Fragment {
 
         }
     };
+    private IARegion.Listener mRegionListener = new IARegion.Listener() {
+        @Override
+        public void onEnterRegion(IARegion region) {
+            if (region.getType() == IARegion.TYPE_FLOOR_PLAN) {
+                fetchFloorPlan(region.getId());
+            }
+        }
 
-    private TextView mLog;
+        @Override
+        public void onExitRegion(IARegion region) {
+            // leaving a previously entered region
+        }
+    };
+    private IATask<IAFloorPlan> mPendingAsyncResult;
+    private long mDownloadId;
+
+    private BlueDotView mImageView;
 
     public MapFragment() {
         // Required empty public constructor
@@ -66,7 +120,11 @@ public class MapFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "Map creating");
         mLocationManager = IALocationManager.create(getActivity());
+        mResourceManager = IAResourceManager.create(getActivity());
+        mLocationManager.registerRegionListener(mRegionListener);
+        mDownloadManager = (DownloadManager) getActivity().getSystemService(Context.DOWNLOAD_SERVICE);
     }
 
     @Override
@@ -74,14 +132,144 @@ public class MapFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_map, container, false);
-        mLog = view.findViewById(R.id.log_tv);
+        mImageView = view.findViewById(R.id.map_view);
         return view;
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        ensurePermissions();
         mLocationManager.requestLocationUpdates(IALocationRequest.create(), mIALocationListener);
+        mLocationManager.registerRegionListener(mRegionListener);
+        getActivity().registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mLocationManager.removeLocationUpdates(mIALocationListener);
+        mLocationManager.unregisterRegionListener(mRegionListener);
+        getActivity().unregisterReceiver(onComplete);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mLocationManager.destroy();
+    }
+
+    private BroadcastReceiver onComplete = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            long id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, 0L);
+            if (id != mDownloadId) {
+                Log.w(TAG, "Ignore unrelated download");
+                return;
+            }
+            Log.w(TAG, "Image download completed");
+            Bundle extras = intent.getExtras();
+            DownloadManager.Query q = new DownloadManager.Query();
+            q.setFilterById(extras.getLong(DownloadManager.EXTRA_DOWNLOAD_ID));
+            Cursor c = mDownloadManager.query(q);
+
+            if (c.moveToFirst()) {
+                int status = c.getInt(c.getColumnIndex(DownloadManager.COLUMN_STATUS));
+                if (status == DownloadManager.STATUS_SUCCESSFUL) {
+                    // process download
+                    String filePath = c.getString(c.getColumnIndex(
+                            DownloadManager.COLUMN_LOCAL_FILENAME));
+                    showFloorPlanImage(filePath);
+                }
+            }
+            c.close();
+        }
+    };
+
+    private void fetchFloorPlan(String id) {
+        cancelPendingNetworkCalls();
+        final IATask<IAFloorPlan> asyncResult = mResourceManager.fetchFloorPlanWithId(id);
+        mPendingAsyncResult = asyncResult;
+        if (mPendingAsyncResult != null) {
+            mPendingAsyncResult.setCallback(new IAResultCallback<IAFloorPlan>() {
+                @Override
+                public void onResult(IAResult<IAFloorPlan> result) {
+                    Log.d(TAG, "fetch floor plan result:" + result);
+                    if (result.isSuccess() && result.getResult() != null) {
+                        mFloorPlan = result.getResult();
+                        String fileName = mFloorPlan.getId() + ".img";
+                        String filePath = Environment.getExternalStorageDirectory() + "/"
+                                + Environment.DIRECTORY_DOWNLOADS + "/" + fileName;
+                        File file = new File(filePath);
+                        if (!file.exists()) {
+                            DownloadManager.Request request =
+                                    new DownloadManager.Request(Uri.parse(mFloorPlan.getUrl()));
+                            request.setDescription("IndoorAtlas floor plan");
+                            request.setTitle("Floor plan");
+                            // requires android 3.2 or later to compile
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                                request.allowScanningByMediaScanner();
+                                request.setNotificationVisibility(DownloadManager.
+                                        Request.VISIBILITY_HIDDEN);
+                            }
+                            request.setDestinationInExternalPublicDir(Environment.
+                                    DIRECTORY_DOWNLOADS, fileName);
+
+                            mDownloadId = mDownloadManager.enqueue(request);
+                        } else {
+                            showFloorPlanImage(filePath);
+                        }
+                    } else {
+                        // do something with error
+                        if (!asyncResult.isCancelled()) {
+                            Toast.makeText(getActivity(),
+                                    (result.getError() != null
+                                            ? "error loading floor plan: " + result.getError()
+                                            : "access to floor plan denied"), Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    }
+                }
+            }, Looper.getMainLooper()); // deliver callbacks in main thread
+        }
+    }
+
+    private void cancelPendingNetworkCalls() {
+        if (mPendingAsyncResult != null && !mPendingAsyncResult.isCancelled()) {
+            mPendingAsyncResult.cancel();
+        }
+    }
+
+    private void showFloorPlanImage(String filePath) {
+        Log.w(TAG, "showFloorPlanImage: " + filePath);
+        mImageView.setRadius(mFloorPlan.getMetersToPixels() * dotRadius);
+        mImageView.setImage(ImageSource.uri(filePath));
+    }
+
+
+    private void ensurePermissions() {
+        if (ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_CODE_WRITE_EXTERNAL_STORAGE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        switch (requestCode) {
+            case REQUEST_CODE_WRITE_EXTERNAL_STORAGE:
+
+                if (grantResults.length == 0
+                        || grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                    Toast.makeText(getActivity(), "Storage Permission Denied",
+                            Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+
     }
 
 }
